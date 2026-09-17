fe466fad7889233f45aa9294b3cb0ac203534b05
Малюкова Ксения Игоревна <106010241@ACADEMY.TOP>
2026-09-17 11:07:03 +0300

    add new function

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,18 +1,103 @@
+import { useState } from "react";
 import Post from "./Post";
 
 function ProfileCard() {
-    return (
-    <section className="profile-card">
-        <div className="profile">
-            <div className="avatar">avatar</div>
-            <div className="profile-info">
-                <h2>Name</h2>
-                <p>@nickname</p>
-            </div>
-        </div>
+    const [posts, setPosts] = useState([
+        {
+            id: 1,
+            title: "text text text",
+            text: "post post post",
+            author: "Ksusha"
+        },
+        {
+            id: 2,
+            title: "text 2 text 2 text 2",
+            text: "post post post",
+            author: "Max"
+        },
+        {
+            id: 3,
+            title: "text 3 text 3 text 3",
+            text: "post post post",
+            author: "Anton"
+        },
+        {
+            id: 4,
+            title: "text 3 text 3 text 3",
+            text: "post post post",
+            author: "Anton"
+        },
+        {
+            id: 5,
+            title: "text 3 text 3 text 3",
+            text: "post post post",
+            author: "Anton"
+        },
+        {
+            id: 6,
+            title: "text 3 text 3 text 3",
+            text: "post post post",
+            author: "Anton"
+        },
+        {
+            id: 7,
+            title: "text 3 text 3 text 3",
+            text: "post post post",
+            author: "Anton"
+        },
+        {
+            id: 8,
+            title: "text 3 text 3 text 3",
+            text: "post post post",
+            author: "Anton"
+        },
 
-        <Post author="Viktor" title="Study React for frontend"  likes={17} text="какой-то осмысленный текст" />
-    </section>
+    ]);
+
+    const [title, setTitle] = useState("");
+    const [text, setText] = useState("");
+
+    function addPost(event) {
+        event.preventDefault();
+
+        const newPost = {
+            id: Date.now(),
+            title: title,
+            text: text,
+            author: "Ksusha"
+        };
+
+        setPosts([...posts, newPost]);
+
+        setTitle("");
+        setText("");
+    }
+
+    return (
+        <section className="profile-card">
+            <div className="profile">
+                <div className="avatar">avatar</div>
+                <div className="profile-info">
+                    <h2>Name</h2>
+                    <p>@nickname</p>
+                </div>
+            </div>
+
+            <form className="post-form" onSubmit={addPost}>
+                <input type="text" placeholder="Заголовок" value={title} onChange={(event) => setTitle(event.target.value)} />
+                <textarea placeholder="Текст поста" value={text} onChange={(event) => setText(event.target.value)}></textarea>
+                <button type="submit">Опубликовть</button>
+            </form>
+
+            {posts.map((post) => (
+                <Post
+                    key={post.id}
+                    author={post.author}
+                    title={post.title}
+                    text={post.text}
+                />
+            ))}
+        </section>
     )
 }
 
